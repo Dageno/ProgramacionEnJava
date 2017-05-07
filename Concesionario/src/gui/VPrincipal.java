ce7105bbc311bdb13c76f6bf6b313fee1866de50
Daniel Gestino Notario <dageno11@gmail.com>
2017-05-08 01:35:24 +0200

    Add files via upload

--- Concesionario/src/gui/VPrincipal.java
+++ Concesionario/src/gui/VPrincipal.java
@@ -27,6 +27,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Dimension;
 import java.awt.Font;
+
 public class VPrincipal {
 
 	JFrame frame;
@@ -54,67 +55,84 @@ public class VPrincipal {
 	 */
 	public VPrincipal() {
 		initialize();
-//		eventoCerrado();
+		// eventoCerrado();
 	}
 
-//	private void eventoCerrado() {
-//		addWindowListener(new WindowAdapter() {
-//			public void windowClosing(WindowEvent e) {
-//				if (Gestion.getModificado()) {
-//					if (JOptionPane.showConfirmDialog(null, "Has hecho cambios... ¿quieres guardar antes de salir?",
-//							"Festival modificado", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-//						if (Gestion.getFile() == null)
-//							try {
-//								Gestion.guardarComo(Gestion.ficheroNuevo(), VentanaPadre.concesionario);
-//							} catch (IOException e1) {
-//								// TODO Auto-generated catch block
-//								e1.printStackTrace();
-//							}
-//						else
-//							try {
-//								Gestion.guardar(VentanaPadre.concesionario);
-//							} catch (IOException e1) {
-//								// TODO Auto-generated catch block
-//								e1.printStackTrace();
-//							}
-//					}
-//				}
-//			}
-//		});
-//		
-//	
-//		
-//	}
-
+	// private void eventoCerrado() {
+	// addWindowListener(new WindowAdapter() {
+	// public void windowClosing(WindowEvent e) {
+	// if (Gestion.getModificado()) {
+	// if (JOptionPane.showConfirmDialog(null, "Has hecho cambios... ¿quieres
+	// guardar antes de salir?",
+	// "Festival modificado", JOptionPane.YES_NO_OPTION) ==
+	// JOptionPane.YES_OPTION) {
+	// if (Gestion.getFile() == null)
+	// try {
+	// Gestion.guardarComo(Gestion.ficheroNuevo(), VentanaPadre.concesionario);
+	// } catch (IOException e1) {
+	// // TODO Auto-generated catch block
+	// e1.printStackTrace();
+	// }
+	// else
+	// try {
+	// Gestion.guardar(VentanaPadre.concesionario);
+	// } catch (IOException e1) {
+	// // TODO Auto-generated catch block
+	// e1.printStackTrace();
+	// }
+	// }
+	// }
+	// }
+	// });
+	//
+	//
+	//
+	// }
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		
-		
-		
+
 		frame = new JFrame();
 		frame.getContentPane().setSize(new Dimension(9, 9));
-		if(Gestion.getFile() != null)
+		if (Gestion.getFile() != null)
 			frame.setTitle(Gestion.getFile().getName());
 		else
 			frame.setTitle("Sin título");
 		frame.setBounds(100, 100, 450, 300);
-		
-		
-		
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if (Gestion.getModificado()) {
+					if (Gestion.deseaGuardar("Se han hecho modificaciones, Desea guardar los cambios ? (y/n)")) {
+						if (Gestion.getFile() == null)
+							try {
+								Gestion.guardarComo(Gestion.ficheroNuevo(), VentanaPadre.concesionario);
+							} catch (IOException e1) {
+								JOptionPane.showMessageDialog(frame, "Se ha producido un error al guardar el archivo.",
+										"Error", JOptionPane.ERROR_MESSAGE);
+							}
+						else
+							try {
+								Gestion.guardar(VentanaPadre.concesionario);
+							} catch (IOException e1) {
+								JOptionPane.showMessageDialog(frame, "Se ha producido un error al guardar el archivo.",
+										"Error", JOptionPane.ERROR_MESSAGE);
+							}
+					}
+				}
+			}
+		});
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu mnArchivo = new JMenu("Archivo");
 		menuBar.add(mnArchivo);
-		
+
 		AltaCoche alta = new AltaCoche();
 		BajaCoche baja = new BajaCoche();
-		
-		
+
 		JMenuItem mntmNuevoConcesionario = new JMenuItem("Nuevo Concesionario");
 		mntmNuevoConcesionario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -122,17 +140,19 @@ public class VPrincipal {
 				try {
 					Gestion.nuevo(VentanaPadre.concesionario);
 				} catch (FileNotFoundException e) {
-					JOptionPane.showMessageDialog(frame, "No se encuentra el archivo.", "Error", JOptionPane.ERROR_MESSAGE);
-					
+					JOptionPane.showMessageDialog(frame, "No se encuentra el archivo.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+
 				} catch (Exception e) {
-					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				}
 
 			}
 		});
 		mntmNuevoConcesionario.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
 		mnArchivo.add(mntmNuevoConcesionario);
-		
+
 		JMenuItem mntmAbrirConcesionario = new JMenuItem("Abrir Concesionario...");
 		mntmAbrirConcesionario.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
 		mntmAbrirConcesionario.addActionListener(new ActionListener() {
@@ -141,21 +161,21 @@ public class VPrincipal {
 					File archivo = leerArchivo();
 					VentanaPadre.concesionario = Gestion.abrir(archivo, VentanaPadre.concesionario);
 					frame.setTitle(archivo.getName());
-					
+
 				} catch (ClassNotFoundException e1) {
 					JOptionPane.showMessageDialog(frame.getContentPane(),
-							"El fichero no contiene ningún concesionario.",
-							"Error", JOptionPane.ERROR_MESSAGE);
-				}catch (Exception e1) {
-					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error", JOptionPane.ERROR_MESSAGE);
+							"El fichero no contiene ningún concesionario.", "Error", JOptionPane.ERROR_MESSAGE);
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		mnArchivo.add(mntmAbrirConcesionario);
-		
+
 		JSeparator separator = new JSeparator();
 		mnArchivo.add(separator);
-		
+
 		JMenuItem mntmGuardar = new JMenuItem("Guardar");
 		mntmGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -163,13 +183,14 @@ public class VPrincipal {
 					Gestion.guardar(VentanaPadre.concesionario);
 					JOptionPane.showMessageDialog(frame, "Se ha guardado el archivo con éxito.");
 				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		mntmGuardar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, InputEvent.CTRL_MASK));
 		mnArchivo.add(mntmGuardar);
-		
+
 		JMenuItem mntmGuardarComo = new JMenuItem("Guardar como...");
 		mntmGuardarComo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -179,17 +200,19 @@ public class VPrincipal {
 					frame.setTitle(archivo.getName());
 					JOptionPane.showMessageDialog(frame, "Se ha guardado el archivo con éxito.");
 				} catch (FileNotFoundException e1) {
-					JOptionPane.showMessageDialog(frame, "No se encuentra el archivo.", "Error", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(frame, "No se encuentra el archivo.", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(frame, "Acceso cancelado por el usuario.", "Error",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		mnArchivo.add(mntmGuardarComo);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		mnArchivo.add(separator_1);
-		
+
 		JMenuItem mntmSalir = new JMenuItem("Salir");
 		mntmSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -197,10 +220,10 @@ public class VPrincipal {
 			}
 		});
 		mnArchivo.add(mntmSalir);
-		
+
 		JMenu mnCoche = new JMenu("Coche\r\n");
 		menuBar.add(mnCoche);
-		
+
 		JMenuItem mntmAltaCoche = new JMenuItem("Alta Coche");
 		mntmAltaCoche.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -210,11 +233,11 @@ public class VPrincipal {
 		});
 		mntmAltaCoche.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.SHIFT_MASK));
 		mnCoche.add(mntmAltaCoche);
-		
+
 		JMenuItem mntmBajaCoche = new JMenuItem("Baja Coche");
 		mntmBajaCoche.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(concesionarioNoVacio()){
+				if (concesionarioNoVacio()) {
 					baja.setModal(true);
 					baja.setVisible(true);
 				}
@@ -222,11 +245,11 @@ public class VPrincipal {
 		});
 		mntmBajaCoche.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, InputEvent.SHIFT_MASK));
 		mnCoche.add(mntmBajaCoche);
-		
+
 		JMenuItem mntmBuscarCoche = new JMenuItem("Buscar Coche");
 		mntmBuscarCoche.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(concesionarioNoVacio()){
+				if (concesionarioNoVacio()) {
 					BuscarCoche buscar = new BuscarCoche();
 					buscar.setModal(true);
 					buscar.setVisible(true);
@@ -235,40 +258,40 @@ public class VPrincipal {
 		});
 		mntmBuscarCoche.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.SHIFT_MASK));
 		mnCoche.add(mntmBuscarCoche);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		mnCoche.add(separator_2);
-		
+
 		JMenuItem mntmMostrarConcesionario = new JMenuItem("Mostrar Concesionario");
 		mntmMostrarConcesionario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(concesionarioNoVacio()){
+				if (concesionarioNoVacio()) {
 					MostrarConcesionario mostrar = new MostrarConcesionario();
 					mostrar.setModal(true);
 					mostrar.setVisible(true);
 				}
 			}
 		});
-		
+
 		JMenuItem mntmBuscarPorColor = new JMenuItem("Buscar Por Color");
 		mntmBuscarPorColor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(concesionarioNoVacio()){
+				if (concesionarioNoVacio()) {
 					BuscarColor buscar = new BuscarColor();
 					buscar.setModal(true);
 					buscar.setVisible(true);
 				}
-				
+
 			}
 		});
 		mntmBuscarPorColor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.SHIFT_MASK));
 		mnCoche.add(mntmBuscarPorColor);
 		mntmMostrarConcesionario.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.ALT_MASK));
 		mnCoche.add(mntmMostrarConcesionario);
-		
+
 		JMenu mnAyuda = new JMenu("Ayuda");
 		menuBar.add(mnAyuda);
-		
+
 		JMenuItem mntmVerAyuda = new JMenuItem("Ver Ayuda");
 		mntmVerAyuda.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -277,7 +300,7 @@ public class VPrincipal {
 			}
 		});
 		mnAyuda.add(mntmVerAyuda);
-		
+
 		JMenuItem mntmAcercaDe = new JMenuItem("Acerca del Concesionario");
 		mntmAcercaDe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -288,38 +311,34 @@ public class VPrincipal {
 		});
 		mnAyuda.add(mntmAcercaDe);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Bienvenido al Concesionario de IES Gran Capit\u00E1n");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 		lblNewLabel.setBounds(57, 76, 335, 76);
 		frame.getContentPane().add(lblNewLabel);
-		
-		
-		}
-	private boolean concesionarioNoVacio(){
-		if(!VentanaPadre.concesionario.isEmpty())
+
+	}
+
+	private boolean concesionarioNoVacio() {
+		if (!VentanaPadre.concesionario.isEmpty())
 			return true;
 		else
-			JOptionPane.showMessageDialog(frame, "El concesionario esta vacío", 
-					"Error", JOptionPane.ERROR_MESSAGE );
+			JOptionPane.showMessageDialog(frame, "El concesionario esta vacío", "Error", JOptionPane.ERROR_MESSAGE);
 		return false;
 	}
-	
+
 	private File leerArchivo() {
-		
+
 		fileChooser.setFileFilter(filtro);
 		int returnVal = fileChooser.showOpenDialog(frame);
 		if (returnVal != JFileChooser.CANCEL_OPTION) {
 			return fileChooser.getSelectedFile();
-			
-		}else{
-			
+
+		} else {
+
 			return null;
-		}	
+		}
 	}
-	
-	
+
 }
-
-
